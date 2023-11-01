a47865ebb54cea7137b929dd842e51f23bf7e13c
Francisco Estrada <52686336+Festrada21@users.noreply.github.com>
2023-11-01 03:31:01 +0000

    configuracions de app mvc con dotnet y ef mssql

---
+++ App/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ServiciosCableInternet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
